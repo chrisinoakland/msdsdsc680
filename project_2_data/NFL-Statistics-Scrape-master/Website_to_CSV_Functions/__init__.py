21d7cc1b6b6e8f7485bc76362f9a9a55902c1859
Christopher Anderson <48421657+chrisinoakland@users.noreply.github.com>
2021-02-06 15:27:00 -0600

    Delete project_2_data directory

--- project_2_data/NFL-Statistics-Scrape-master/Website_to_CSV_Functions/__init__.py
+++
@@ -1,6 +0,0 @@
-from Website_to_CSV_Functions.Basic_Stats_Class import *
-from Website_to_CSV_Functions.Career_Stats_Class import *
-from Website_to_CSV_Functions.Game_Logs_Class import *
-from Website_to_CSV_Functions.Obtain_Players_from_Website import *
-from Website_to_CSV_Functions.NFL_Glossary import *
-from Website_to_CSV_Functions.Functions_Needed_For_All_Stats import *
